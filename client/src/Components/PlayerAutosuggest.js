f4876f968f42638500a3fb857b9ec07279bceb62
ᴋɪʀʙʏ ᴋᴏʜʟᴍᴏʀɢᴇɴ <kirby@kkohlmorgen.com>
2017-11-01 18:52:33 -0400

    It works\!

--- client/src/Components/PlayerAutosuggest.js
+++ client/src/Components/PlayerAutosuggest.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import Button from 'material-ui/Button';
-import { css } from 'emotion';
-import TextField from 'material-ui/TextField';
-import axios from 'axios'
+import PropTypes from 'prop-types';
 import Autosuggest from 'react-autosuggest';
+import TextField from 'material-ui/TextField';
 import Paper from 'material-ui/Paper';
 import { MenuItem } from 'material-ui/Menu';
 import match from 'autosuggest-highlight/match';
 import parse from 'autosuggest-highlight/parse';
 import { withStyles } from 'material-ui/styles';
+
+
+function renderInput(inputProps) {
+  const { autoFocus, id, label, value, ...other } = inputProps;
+
+  return (
+    <TextField
+      autoFocus={autoFocus}
+      value={value}
+      margin="dense"
+      id={id}
+      label={label}
+      InputProps={{
+        ...other,
+      }}
+      fullWidth
+    />
+  );
+}
+
+function renderSuggestion(suggestion, { query, isHighlighted }) {
+  const matches = match(suggestion.name, query);
+  const parts = parse(suggestion.name, matches);
+
+  return (
+    <MenuItem selected={isHighlighted} component="div">
+      <div>
+        {parts.map((part, index) => {
+          return part.highlight ? (
+            <span key={index} style={{ fontWeight: 300 }}>
+              {part.text}
+            </span>
+          ) : (
+            <strong key={index} style={{ fontWeight: 500 }}>
+              {part.text}
+            </strong>
+          );
+        })}
+      </div>
+    </MenuItem>
+  );
+}
 
 function renderSuggestionsContainer(options) {
   const { containerProps, children } = options;
@@ -20,39 +60,18 @@ function renderSuggestionsContainer(options) {
   );
 }
 
-export default class PlayerAutosuggest extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      suggestions: [],
-      value: '',
-    };
-  }
+function getSuggestionValue(suggestion) {
+  return suggestion.name;
+}
 
-  componentDidMount() {
-    axios.get('http://kirby.ngrok.io/players/suggestions')
-      .then(response => {
-        this.setState({
-          suggestions: response.data.suggestions
-        });
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  }
+function getSuggestions(suggestions, value) {
+  const inputValue = value.trim().toLowerCase();
+  const inputLength = inputValue.length;
+  let count = 0;
 
-  getSuggestionValue(suggestion) {
-    return suggestion.name;
-  }
-
-  getSuggestions(value) {
-    const inputValue = value.trim().toLowerCase();
-    const inputLength = inputValue.length;
-    let count = 0;
-
-    return inputLength === 0
-      ? []
-      : this.state.suggestions.filter(suggestion => {
+  return inputLength === 0
+    ? []
+    : suggestions.filter(suggestion => {
         const keep =
           count < 5 && suggestion.name.toLowerCase().slice(0, inputLength) === inputValue;
 
@@ -62,27 +81,43 @@ export default class PlayerAutosuggest extends React.Component {
 
         return keep;
       });
-  }
+}
 
-  renderInput(inputProps) {
-    const { autoFocus, value, id, label } = inputProps;
+const styles = theme => ({
+  container: {
+    flexGrow: 1,
+    position: 'relative',
+  },
+  suggestionsContainerOpen: {
+    position: 'absolute',
+    marginTop: theme.spacing.unit,
+    marginBottom: theme.spacing.unit * 3,
+    left: 0,
+    right: 0,
+    zIndex: 9999,
+  },
+  suggestion: {
+    display: 'block',
+  },
+  suggestionsList: {
+    margin: 0,
+    padding: 0,
+    listStyleType: 'none',
+  },
+  textField: {
+    width: '100%',
+  },
+});
 
-    return (
-      <TextField
-        autoFocus={autoFocus}
-        margin="dense"
-        value={value}
-        id={id}
-        label={label}
-        type="text"
-        fullWidth
-      />
-    );
-  }
+class IntegrationAutosuggest extends React.Component {
+  state = {
+    value: '',
+    suggestions: [],
+  };
 
   handleSuggestionsFetchRequested = ({ value }) => {
     this.setState({
-      suggestions: this.getSuggestions(value),
+      suggestions: getSuggestions(this.props.suggestions, value),
     });
   };
 
@@ -98,43 +133,29 @@ export default class PlayerAutosuggest extends React.Component {
     });
   };
 
-  renderSuggestion(suggestion, { query, isHighlighted }) {
-    const matches = match(suggestion.name, query);
-    const parts = parse(suggestion.name, matches);
-
-    return (
-      <MenuItem selected={isHighlighted} component="div">
-        <div>
-          {parts.map((part, index) => {
-            return part.highlight ? (
-              <span key={index} style={{ fontWeight: 300 }}>
-                {part.text}
-              </span>
-            ) : (
-              <strong key={index} style={{ fontWeight: 500 }}>
-                {part.text}
-              </strong>
-            );
-          })}
-        </div>
-      </MenuItem>
-    );
-  }
-
   render() {
+    const { classes } = this.props;
+
     return (
       <Autosuggest
-        renderInputComponent={this.renderInput}
+        theme={{
+          container: classes.container,
+          suggestionsContainerOpen: classes.suggestionsContainerOpen,
+          suggestionsList: classes.suggestionsList,
+          suggestion: classes.suggestion,
+        }}
+        animated
+        renderInputComponent={renderInput}
         suggestions={this.state.suggestions}
         onSuggestionsFetchRequested={this.handleSuggestionsFetchRequested}
         onSuggestionsClearRequested={this.handleSuggestionsClearRequested}
         renderSuggestionsContainer={renderSuggestionsContainer}
-        getSuggestionValue={this.getSuggestionValue}
-        renderSuggestion={this.renderSuggestion}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
         inputProps={{
-          autoFocus: true,
-          id: 'player1',
-          label: 'Player 1',
+          autoFocus: this.props.autoFocus,
+          id: this.props.id,
+          label: this.props.label,
           value: this.state.value,
           onChange: this.handleChange,
         }}
@@ -142,3 +163,5 @@ export default class PlayerAutosuggest extends React.Component {
     );
   }
 }
+
+export default withStyles(styles)(IntegrationAutosuggest);
